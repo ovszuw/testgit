a4e7f7c475a1f38ea6e69b9c7ed32c50f93d076f
aovs <ovszuw@qq.com>
2020-03-27 14:57:36 +0800

    新增念一千遍阿弥陀佛

---
+++ scripts/f.py
@@ -0,0 +1,11 @@
+import time
+for _ in range(1000):
+    print ('念第%d遍:' % (_ + 1))
+    print ('阿', end = '')
+    time.sleep(0.382)
+    print ('弥', end = '')
+    time.sleep(0.382)
+    print ('陀', end = '')
+    time.sleep(0.382)
+    print ('佛')
+    time.sleep(0.618)
